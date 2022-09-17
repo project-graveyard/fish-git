3cfe71038813c34a0210864eb992a071b5fbcccd
David Saah <dasorange.hope@gmail.com>
2022-09-17 23:52:25 +0000

    add git log graph alias

--- functions/glg.fish
+++ functions/glg.fish
@@ -1,4 +1,4 @@
 # Defined via `source`
-function glg --wraps='git log --stat' --description 'alias glg git log --stat'
-  git log --stat $argv; 
+function glg --wraps='git log --graph' --description 'alias glg git log --graph'
+  git log --graph $argv;
 end
