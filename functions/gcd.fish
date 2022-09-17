3a33d7de6ceb7f8295f02cc2ae1c6e71411a33ca
David Saah <dasorange.hope@gmail.com>
2022-09-17 23:26:18 +0000

    delete files

--- functions/gcd.fish
+++
@@ -1,4 +0,0 @@
-# Defined via `source`
-function gcd --wraps='git checkout (git_develop_branch)' --description 'alias gcd git checkout (git_develop_branch)'
-  git checkout (git_develop_branch) $argv; 
-end
--- functions/git_inside_worktree.fish
+++
@@ -1,3 +0,0 @@
-function git_inside_worktree
-    return (git rev-parse --is-inside-work-tree > /dev/null 2> /dev/null)
-end
