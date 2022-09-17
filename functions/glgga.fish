0c74c74576fe5d335ab42ea14b7b41fc9ccd1203
David Saah <dasorange.hope@gmail.com>
2022-09-17 23:59:26 +0000

    delete files

--- functions/glgga.fish
+++
@@ -1,4 +0,0 @@
-# Defined via `source`
-function glgga --wraps='git log --graph --decorate --all' --description 'alias glgga git log --graph --decorate --all'
-  git log --graph --decorate --all $argv; 
-end
--- functions/glgm.fish
+++
@@ -1,4 +0,0 @@
-# Defined via `source`
-function glgm --wraps='git log --graph --max-count=10' --description 'alias glgm git log --graph --max-count=10'
-  git log --graph --max-count=10 $argv; 
-end
